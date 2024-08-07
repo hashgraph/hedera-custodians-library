53fd8fbcc135b7f671ea7c9e3b6db80e519a0286
Mario Francia <mario@io.builders>
2024-08-07 15:55:56 +0200

    feat: added kms sign
    Signed-off-by: Mario Francia <mario@io.builders>

--- src/strategies/signature/AWSKMSStrategy.ts
+++ src/strategies/signature/AWSKMSStrategy.ts
@@ -1,32 +1,43 @@
-/*
- *
- * Hedera Custodians Integration
- *
- * Copyright (C) 2023 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 import { ISignatureStrategy } from './ISignatureStrategy';
 import { AWSKMSConfig } from '../config/AWSKMSConfig';
 import { SignatureRequest } from '../../models/signature/SignatureRequest';
+import { KMSClient, SignCommand, SignCommandInput } from '@aws-sdk/client-kms';
+
+const SIGNING_ALGORITHM = "ECDSA_SHA_256";
+const MESSAGE_TYPE = "DIGEST";
+
+let kmsClient: KMSClient;
 
 export class AWSKMSStrategy implements ISignatureStrategy {
-  constructor(strategyConfig: AWSKMSConfig) {}
+  private readonly strategyConfig: AWSKMSConfig;
+
+  constructor(strategyConfig: AWSKMSConfig) {
+    this.strategyConfig = strategyConfig;
+    kmsClient = new KMSClient({
+      region: this.strategyConfig.awsRegion,
+      credentials: {
+        accessKeyId: this.strategyConfig.awsAccessKeyId,
+        secretAccessKey: this.strategyConfig.awsSecretAccessKey,
+      },
+    });
+  }
 
   async sign(request: SignatureRequest): Promise<Uint8Array> {
-    //TODO: add the logics to sign
-    return new Uint8Array();
+    const signingInput: SignCommandInput = {
+      KeyId: process.env.AWS_KMS_KEY_ID!,
+      SigningAlgorithm: SIGNING_ALGORITHM,
+      MessageType: MESSAGE_TYPE,
+      Message: request.getTransactionBytes(),
+    };
+
+    const command = new SignCommand(signingInput);
+    const response = await kmsClient.send(command);
+
+    if (!response.Signature) {
+      throw new Error("Signature not found");
+    }
+
+    return new Uint8Array(response.Signature);
   }
+
 }
